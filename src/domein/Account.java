89411911177837c5d1e513eddeda9f7e37e916e6
michael-dv <michaeldevreeze@gmail.com>
2012-10-02 13:14:04 +0200

    merge

---
+++ src/domein/Account.java
@@ -0,0 +1,85 @@
+package domein;
+
+public class Account {
+
+}
+package domein;
+
+public class Account {
+
+	private int id;
+	private String emailadres;
+	private String wachtwoord;
+	private String voornaam;
+	private String tussenvoegsel;
+	private String achternaam;
+	private String postcode;
+	private String telefoonnummer;
+	private String straat;
+	private int huisnummer;
+	private String plaats;
+	
+	public String getEmailadres() {
+		return emailadres;
+	}
+	public void setEmailadres(String emailadres) {
+		this.emailadres = emailadres;
+	}
+	public String getWachtwoord() {
+		return wachtwoord;
+	}
+	public void setWachtwoord(String wachtwoord) {
+		this.wachtwoord = wachtwoord;
+	}
+	public String getVoornaam() {
+		return voornaam;
+	}
+	public void setVoornaam(String voornaam) {
+		this.voornaam = voornaam;
+	}
+	public String getTussenvoegsel() {
+		return tussenvoegsel;
+	}
+	public void setTussenvoegsel(String tussenvoegsel) {
+		this.tussenvoegsel = tussenvoegsel;
+	}
+	public String getAchternaam() {
+		return achternaam;
+	}
+	public void setAchternaam(String achternaam) {
+		this.achternaam = achternaam;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+	public String getTelefoonnummer() {
+		return telefoonnummer;
+	}
+	public void setTelefoonnummer(String telefoonnummer) {
+		this.telefoonnummer = telefoonnummer;
+	}
+	public String getStraat() {
+		return straat;
+	}
+	public void setStraat(String straat) {
+		this.straat = straat;
+	}
+	public int getHuisnummer() {
+		return huisnummer;
+	}
+	public void setHuisnummer(int huisnummer) {
+		this.huisnummer = huisnummer;
+	}
+	public String getPlaats() {
+		return plaats;
+	}
+	public void setPlaats(String plaats) {
+		this.plaats = plaats;
+	}
+	
+	
+	
+}
