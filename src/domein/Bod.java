4933aeb1bee6e32a08f7644739dbd85d23a387d2
Igor <icelance@hotmail.ru>
2012-10-30 14:42:59 +0100

    Bod ++

--- src/domein/Bod.java
+++ src/domein/Bod.java
@@ -8,7 +8,25 @@ public class Bod {
 
   public Double bod;
 
-  public Date datumTijd;
+  public Double getBod() {
+	return bod;
+}
+public void setBod(Double bod) {
+	this.bod = bod;
+}
+public Date getDatumTijd() {
+	return datumTijd;
+}
+public void setDatumTijd(Date datumTijd) {
+	this.datumTijd = datumTijd;
+}
+public Veiling getVeiling() {
+	return veiling;
+}
+public void setVeiling(Veiling veiling) {
+	this.veiling = veiling;
+}
+public Date datumTijd;
 
     public Account account;
     public Veiling veiling;
